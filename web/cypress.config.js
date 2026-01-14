237ccdcdc92ca8e708398090fe0ced815ea1953b
Pamella.candeia <pamella.candeia1999@gmail.com>
2026-01-13 22:54:11 -0300

    13-01-2026

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -3,7 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.name === "chrome") {
+          launchOptions.args.push(
+            '--user-data-dir=C:\\cypress-chrome-profile',
+            "--profile-directory=Default"
+          );
+        }
+        return launchOptions;
+      });
+
+      return config;
     },
     // defaultCommandTimeout: 10000
     experimentalStudio: true
--- web/cypress/e2e/studio.cy.js
+++ web/cypress/e2e/studio.cy.js
@@ -1,20 +1,20 @@
 describe('studio', () => {
   it('Exemplo do cypress studio', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://web.whatsapp.com/')
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('h1').should('be.visible');
-    cy.get('h1').should('have.text', 'Kitchen Sink');
+    // cy.get('h1').should('be.visible');
+    // cy.get('h1').should('have.text', 'Kitchen Sink');
     /* ==== End Cypress Studio ==== */
   })
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('Deve logar com sucesso', function() {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000');
-    cy.get('#email').type('papito@webdojo.com');
-    cy.get('#password').type('katana123');
-    cy.get('.bg-\\[\\#8257E5\\]').click();
-    cy.get('[data-cy="user-name"]').should('have.text', 'Fernando Papito');
-    /* ==== End Cypress Studio ==== */
-  });
+  // /* ==== Test Created with Cypress Studio ==== */
+  // it('Deve logar com sucesso', function() {
+  //   /* ==== Generated with Cypress Studio ==== */
+  //   cy.visit('http://localhost:3000');
+  //   cy.get('#email').type('papito@webdojo.com');
+  //   cy.get('#password').type('katana123');
+  //   cy.get('.bg-\\[\\#8257E5\\]').click();
+  //   cy.get('[data-cy="user-name"]').should('have.text', 'Fernando Papito');
+  //   /* ==== End Cypress Studio ==== */
+  // });
 })
